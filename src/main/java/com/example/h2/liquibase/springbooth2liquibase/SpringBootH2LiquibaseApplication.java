b6180beb59eec31b30f0fd7f805eac877bfe5fcf
Ema Fazillah binti Abu Bakar <ema.fazillah.abu@rhbgroup.com>
2018-08-17 10:53:00 +0800

    Create spring boot project, h2 & liquibase

---
+++ src/main/java/com/example/h2/liquibase/springbooth2liquibase/SpringBootH2LiquibaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.h2.liquibase.springbooth2liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2LiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2LiquibaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/h2/liquibase/springbooth2liquibase/SpringBootH2LiquibaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.h2.liquibase.springbooth2liquibase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootH2LiquibaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
